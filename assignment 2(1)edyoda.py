eb8040f5c041980db941ba1d871cfff4a090712b
shristi2929 <95220373+shristi2929@users.noreply.github.com>
2022-07-27 00:22:20 +0530

    Add files via upload

---
+++ assignment 2(1)edyoda.py
@@ -0,0 +1,9 @@
+l=[(2,5),(1,2),(4,4),(2,3),(2,1)]
+for i in range(len(1)):
+    for j in range(len(1)):
+        if l[i][1]<l[j][1]:
+            l[i],l[j]=l[j],l[i]
+print(l,end="\n\t thank you sir\t\t")
+          
+
+
